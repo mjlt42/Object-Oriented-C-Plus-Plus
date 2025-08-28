b92df2690445d6498878c47763814b2b1b722ac0
mjlt42 <tarskyman42@gmail.com>
2025-08-27 22:26:05 -0700

    test
    test

---
+++ Hello.cpp
@@ -0,0 +1,12 @@
+/***
+ * Prints Hello world, twice
+ */
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
