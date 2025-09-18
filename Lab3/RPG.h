72a3c71ee4cd756c60e75d40b26ac7b75bd4a05d
mjlt42 <tarskyman42@gmail.com>
2025-09-18 15:32:20 -0700

    Created new files and updated header
    Created:
    main.cpp
    RPG.cpp
    RPG.h (and modiefied)

---
+++ Lab3/RPG.h
@@ -0,0 +1,36 @@
+//RPG.H
+#ifndef RPG_H
+#define RPG_H
+#include <string>
+
+using namespace std;
+const int INVENTORY_SIZE = 10;
+const float HIT_FACTOR = 0.05;
+const int MAX_HITS_TAKEN = 3;
+class RPG
+{
+private:
+
+    string name;
+    int hits_taken;
+    float luck;
+    float exp;
+    int level;
+    
+public:
+    //Constructors
+    RPG();
+    RPG(string name, int hits_taken, float luck, float exp, int level);
+
+    //mutators
+    bool isAlive() const;
+    void setHitsTaken(int new_hits);
+
+    //accessors
+    const string getName();
+    const int getHitsTaken();
+    const float getLuck();
+    const float getExp();
+    const int getLevel();
+};
+#endif
