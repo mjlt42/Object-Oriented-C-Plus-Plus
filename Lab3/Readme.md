85ad565fa79a3254895d46e98ee85d2a0259463d
mjlt42 <tarskyman42@gmail.com>
2025-09-22 22:28:59 -0700

    Updated main.cpp and readme w/ screenshot

---
+++ Lab3/Readme.md
@@ -0,0 +1,8 @@
+# Lab 3 - Class Objects
+
+Used the RPG object to intially setup text based RPG game
+
+## Screenshot of main.cpp output
+
+![alt text](images/mainOutput.png)
+
---
+++ Lab3/main.cpp
@@ -0,0 +1,36 @@
+//main.cpp
+#include <iostream>
+#include <string>
+#include "RPG.h"
+using namespace std;
+
+int main (){
+
+RPG p1 = RPG("Wiz", 0, 0.2, 60, 1);
+RPG p2 = RPG("Knight", 0, 0.1, 70, 1);
+
+printf("%s Current Stats\n", p1.getName().c_str());
+printf("Hits Taken: %i\t Luck: %f\t Exp: %f\t Level: %i\n", p1.getHitsTaken(), p1.getLuck(), p1.getExp(), p1.getLevel());
+printf("\n");
+//Print stats for p2
+printf("%s Current Stats\n", p2.getName().c_str());
+printf("Hits Taken: %i\t Luck: %f\t Exp: %f\t Level: %i\n", p2.getHitsTaken(), p2.getLuck(), p2.getExp(), p2.getLevel());
+
+//call setHitsTaken on either player
+
+p2.setHitsTaken(1);
+
+//print out hits taken
+
+printf("\n%s hits taken: %i\n", p1.getName().c_str(), p1.getHitsTaken());
+printf("%s hits taken: %i\n\n", p2.getName().c_str(), p2.getHitsTaken());
+
+// call isAlive on both p1 and p2
+
+printf("0 is dead, 1 is alive:\n");
+printf("%s is %i\n", p1.getName().c_str(), p1.isAlive());
+printf("%s is %i\n", p2.getName().c_str(), p2.isAlive());
+
+return 0;
+
+}
