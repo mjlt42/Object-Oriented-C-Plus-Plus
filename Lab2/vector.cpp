b1f2eb15177f42f290c7e11bdd8b52833d16248c
mjlt42 <tarskyman42@gmail.com>
2025-09-22 20:33:06 -0700

    Update vector.cpp

--- Lab2/vector.cpp
+++ Lab2/vector.cpp
@@ -9,7 +9,7 @@ using namespace std;
  * @param v - vector of integers
  */
 
- void printMemArr(vector<int> v){
+ void printMemVec(vector<int> v){
     printf("Vector - Each int is worth %lu bytes\n", v.size());
     for(int i = 0; i < v.size(); i++){
         printf("Value: %i at Memory Location: %p\n", v[i], &v);
@@ -22,10 +22,10 @@ using namespace std;
   * 
   * @param v - address to a vector of integers
   */
-void incArrBy10(vector<int> &v){
+void incVecBy10(vector<int> &v){
    for(int i = 0; i < v.size(); i++)
    {
-    v[i]= v[i] += 10;
+    v[i] += 10;
    }
 }
 
@@ -36,29 +36,38 @@ int main(){
 //create a vector of integers called vec that can hold up to 5 elements
     vector<int> vec(SIZE);
 //use a for loop to populate vec with the values 100 to 104
-
+    for(int i = 0; i < SIZE; i++)
+   {
+    vec.push_back(100 + i);
+   }
 
 printf("Before Increment------------\n");
 //call printMemVec(...) on vec
-
+    printMemVec(vec);
 //call inBy10(...) on vec
+    incVecBy10(vec);
+
 
 
 printf("After Increment-------------\n");
 //call printMemVec(...) on vec to view changes
-
+   printMemVec(vec);
 
 // remove the last element of vec
+   vec.pop_back();
 //refer to doc
 
 printf("After Pop-------------------\n");
 //call printMemVec(...) on vec to view changes
-
+   printMemVec(vec);
 
 //append 101 and 102 at the end of vec
 //refer to doc
-
+   vec.push_back(101);
+   vec.push_back(102);
 printf("After Push-------------------\n");
 //call printMemVec(...) on vec to view changes
+    printMemVec(vec);
     return 0;
 }
+
